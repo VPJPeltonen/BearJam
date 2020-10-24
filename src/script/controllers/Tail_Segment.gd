04602a341ba2e242dfcd09ed61b6a5a76322dc09
VPJP <vpjpeltonen@gmail.com>
2020-10-24 16:38:16 +0300

    worm takes damage

--- src/script/controllers/Tail_Segment.gd
+++ src/script/controllers/Tail_Segment.gd
@@ -5,23 +5,33 @@ export(PackedScene) var tail_segment
 var dir = Vector3(0,0,1)
 var current_rot = 0
 var mesh_dirs = [Vector3(0,0,0),Vector3(0,90,0),Vector3(0,180,0),Vector3(0,270,0)]
+var previous_pos
 
 var tail
+var head
 
 func init(host):
 	global_transform.origin = host.get_tail_spawn_pos()
+	head = host
 
 func move(pos,previous_dir):
 	var old_dir = current_rot
 	current_rot = previous_dir
 	rotation_degrees = mesh_dirs[current_rot]
-	var old_pos = global_transform.origin
+	previous_pos = global_transform.origin
 	global_transform.origin = pos
 	if tail != null:
-		tail.move(old_pos,old_dir)
-
+		tail.move(previous_pos,old_dir)
+		
+func damage():
+	if tail == null:
+		head.tail = null
+		queue_free()
+	else:
+		tail.damage()
+	
 func get_tail_spawn_pos():
-	return global_transform.origin-(dir*GAME.grid_size)
+	return previous_pos
 
 func add_tail():
 	if tail == null:
