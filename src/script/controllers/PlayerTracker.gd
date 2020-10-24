e652ebff7e551bdd47b1cf998af97356e03e93aa
VPJP <vpjpeltonen@gmail.com>
2020-10-24 15:26:49 +0300

    camera adjustments

--- src/script/controllers/PlayerTracker.gd
+++ src/script/controllers/PlayerTracker.gd
@@ -2,7 +2,7 @@ extends Position3D
 
 var track_speed = 1.0
 
-onready var snek = get_parent().get_node("Snek")
+onready var snek = get_parent().get_node("Snek/View_Pos")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
