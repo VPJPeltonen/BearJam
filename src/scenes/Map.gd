2060da4134e07b29641397f501265b93bb794913
VPJP <vpjpeltonen@gmail.com>
2020-10-25 19:23:12 +0200

    basic map layout

--- src/scenes/Map.gd
+++ src/scenes/Map.gd
@@ -2,5 +2,8 @@ extends Spatial
 
 signal restart
 
+func start_game():
+	$Beat.start()
+
 func _on_LoseScreen_reset():
 	emit_signal("restart")
--- src/script/controllers/Main.gd
+++ src/script/controllers/Main.gd
@@ -1,6 +1,7 @@
 extends Spatial
 
 export(PackedScene) var test_map
+export(PackedScene) var final_map
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,12 +9,34 @@ func _ready():
 
 func restart():
 	GAME.reset()
-	var children = get_children()
+	$Node2D.show()
+	var children = $Map.get_children()
 	for child in children:
 		child.queue_free()
-	var new_map = test_map.instance()
-	add_child(new_map)
+	var new_map = final_map.instance()
+	$Map.add_child(new_map)
 	new_map.connect("restart", self, "_on_TestMap_restart")
 
 func _on_TestMap_restart():
 	restart()
+
+func _on_StartButton_pressed():
+	$Node2D.hide()
+	var map = $Map.get_children()[0]
+	map.start_game()
+
+
+func _on_ExitButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_CreditsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_ControlsButton_pressed():
+	pass # Replace with function body.
+
+
+func _on_TestMap2_restart():
+	pass # Replace with function body.
